9765dc69aafa930d9ede12739b5a96b9e79285d5
Daniel Brito de Souza <danielb.desouza1403@gmail.com>
2026-05-30 18:27:18 -0300

    add: middleware para validar inputs do prato

--- backend/src/routes/pratoRoutes.js
+++ backend/src/routes/pratoRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pratoController = require('../controller/pratoController');
 const autenticacao = require('../middleware/auth');
+const validarPrato = require('../middleware/validarPrato');
 
-router.post('/cadastrar-prato', autenticacao, pratoController.cadastrar_prato);
-router.put('/atualizar-prato/:id', autenticacao, pratoController.atualizar_prato);
+router.post('/cadastrar-prato', autenticacao, validarPrato, pratoController.cadastrar_prato);
+router.put('/atualizar-prato/:id', autenticacao, validarPrato, pratoController.atualizar_prato);
 router.delete('/deletar-prato/:id', autenticacao, pratoController.deletar_prato);
 router.get('/listar-pratos', pratoController.listar_pratos);
 router.get('/buscar-prato/:id', pratoController.buscar_prato);
